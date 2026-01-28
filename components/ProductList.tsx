0e86d71f7602b0f87ce156a65994fc3d635c4bd4
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-28 19:15:34 +0800

    Add TypeScript types and improve product mapping
    Add TypeScript types and improve product mapping

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -6,8 +6,13 @@ import { useSelector, useDispatch  } from "react-redux";
 import axios from "axios";
 
 function ProductList() {
-    const [products, setProducts] = useState([]); // from fetchdata
-    
+    const [products, setProducts] = useState<product[]>([]); // from fetchdata
+    interface product {
+        id: number;
+        image: string;
+        price: number;
+        title: string
+    }
     useEffect(()=>{    
             axios.get('https://fakestoreapi.com/products')
             .then(response => setProducts(response.data))
@@ -16,15 +21,15 @@ function ProductList() {
             });
     },[])
     
-    // const {items:products, status} = useSelector((state)=>state.products)
+    //const {items, status} = useSelector((state)=>state.products)
     const dispatch = useDispatch();
        
     return (
         <div>
             <h1>Product List Page</h1>
             {products.map(product => (
-                <div key={product.id}>
-                <img src={product.image} alt="image title" />
+                <div key={product?.id}>
+                <img src={product?.image} alt="image title" />
                 <h2>{product.title.length > 20 ? `${product.title.slice(0, 20)}...` : product.title }</h2>
                 <p>Price : ${product.price}</p>
                 <button onClick={()=>dispatch(addToCart(product))}>Add to cart</button>
@@ -36,14 +41,3 @@ function ProductList() {
 }
 
 export default ProductList
-
-{/*
-    // https://www.geeksforgeeks.org/reactjs/fetching-data-from-an-api-with-useeffect-and-usestate-hook/ 
-        const [userList, setUserList] = useState([]);
-
-        useEffect(() => {
-            fetch('https://random-data-api.com/api/v2/users?size=5')
-                .then(response => response.json())
-                .then(data => setUserList(data));
-        }, []);
-*/}
