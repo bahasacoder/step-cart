abfe2a6264ac67056455a648c3fc79a5aed9f62d
bahasacoder <webcodebahasa@gmail.com>
2026-01-28 22:06:06 +0800

    list 2206

--- lib/store.ts
+++ lib/store.ts
@@ -13,3 +13,9 @@ export const store = configureStore({
   }
 
 });
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof store>
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
