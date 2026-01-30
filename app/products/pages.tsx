a8e498b1af5ea33c2ea28b2357d8fd67d64cdd34
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-30 09:31:11 +0800

    Update pages.tsx products
    export default function Page() {
      return <h1>Hello, Next.js!</h1>
    }

--- app/products/pages.tsx
+++ app/products/pages.tsx
@@ -1,12 +1,3 @@
-"use client"
-import React from 'react';
-import { store, RootState } from '@/lib/store';
-import Shop from '@/components/shop';
-
-const Usage = () => {
-  return (
-      <Shop />
-  );
-};
-
-export default Usage;
+export default function ProductsPage() {
+  return <h1>Hello, Products Paga Next.js!</h1>
+}
