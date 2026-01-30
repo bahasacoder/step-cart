da1aad24c6d9d1b058ad0b4eb6f1beded20a01fa
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-01-30 13:17:36 +0800

    Update page.tsx

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Provider, useSelector } from 'react-redux';
 import { store, RootState } from '@/lib/store';
-import ProductList from '@/components/ProductList';
+import ProductList from '@/components/product-list';
 
 const List = () => {
   return (
