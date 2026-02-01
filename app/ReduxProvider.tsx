1c21c9ed4d635700c27ef9252670a6f220b34a0c
bahasacoder <webcodebahasa@gmail.com>
2026-02-01 13:57:32 +0800

    persist 1357

--- app/ReduxProvider.tsx
+++ app/ReduxProvider.tsx
@@ -2,13 +2,18 @@
 'use client';
 
 import { Provider } from 'react-redux';
-import { store } from '@/lib/store';
+import { persistStore } from "redux-persist";
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '@/lib/store';
+
 
 export default function ReduxProvider({ children, }: {
   children: React.ReactNode;})  {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
             {children}
+      </PersistGate>
     </Provider>
   );  
 }
--- lib/store.ts
+++ lib/store.ts
@@ -1,14 +1,42 @@
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import cartReducer from "@/features/cartSlice";
 import keranjangReducer from "@/features/keranjangSlice";
 
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  keranjang: keranjangReducer
+})
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    keranjang: keranjangReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
