53787d75df4e3a6d5e2068d5c6067322788e871c
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-11 22:03:40 +0800

    Update hero-page.tsx 2203
    Update hero-page.tsx 2203

--- components/blocks/hero/hero-page.tsx
+++ components/blocks/hero/hero-page.tsx
@@ -28,38 +28,38 @@ const navigationData: NavigationSection[] = [
 const menudata = [
   {
     id: 1,
-    img: 'https://cdn.shadcnstudio.com/ss-assets/template/landing-page/bistro/image-18.png',
+    img: ''https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@caf3b27229a7a6040fd0da2db8d6acce5ef66604/uploads/2026-02-11T13-25-48-236Z-r4v4clqxn.jpg',
     imgAlt: 'plate-1',
-    userComment: 'Telur Ayam Kampungnya sangat segar dan rasanya lezat! Sangat direkomendasikan.',
+    userComment: 'Telur Ayam Kampung segar dengan rasanya lezat! Sangat direkomendasikan.',
     userAvatar: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@caf3b27229a7a6040fd0da2db8d6acce5ef66604/uploads/2026-02-11T13-25-48-236Z-r4v4clqxn.jpg'
   },
   {
     id: 2,
-    img: 'https://cdn.shadcnstudio.com/ss-assets/template/landing-page/bistro/image-19.png',
+    img: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@17adf2502b9a3c1995a663317fff7025a21a66c1/uploads/2026-02-11T13-58-09-796Z-x74stcxze.jpg',
     imgAlt: 'plate-2',
     userComment: 'DOC Anak ayam kampung.',
-    userAvatar: 'https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-46.png'
+    userAvatar: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@17adf2502b9a3c1995a663317fff7025a21a66c1/uploads/2026-02-11T13-58-09-796Z-x74stcxze.jpg'
   },
   {
     id: 3,
-    img: 'https://cdn.shadcnstudio.com/ss-assets/template/landing-page/bistro/image-20.png',
+    img: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@8fe9291f11e9c09d575be26f7e91f69d21f77e4c/uploads/2026-02-11T13-59-39-333Z-uwnn3v0st.jpg',
     imgAlt: 'plate-3',
     userComment: 'Anak ayam kampung asli untuk pemelihaaraan lanjutan.',
-    userAvatar: 'https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-57.png'
+    userAvatar: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@8fe9291f11e9c09d575be26f7e91f69d21f77e4c/uploads/2026-02-11T13-59-39-333Z-uwnn3v0st.jpg'
   },
   {
     id: 4,
-    img: 'https://cdn.shadcnstudio.com/ss-assets/template/landing-page/bistro/image-05.png',
+    img: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@3bc973a2d28cbc2cbd694b3aa776be9ce23dab27/uploads/2026-02-11T13-31-06-114Z-yuktmis7e.jpg',
     imgAlt: 'plate-4',
     userComment: 'Ayam Kampung frozen berkualitas tinggi dan rasanya enak sekali!',
     userAvatar: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@3bc973a2d28cbc2cbd694b3aa776be9ce23dab27/uploads/2026-02-11T13-31-06-114Z-yuktmis7e.jpg'
   },
   {
     id: 5,
-    img: 'https://cdn.shadcnstudio.com/ss-assets/template/landing-page/bistro/image-20.png',
+    img: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@843c257eb10ec413f439b015bf51b4833b6e1fe4/uploads/2026-02-11T14-02-47-007Z-twl21bx45.jpg',
     imgAlt: 'plate-3',
     userComment: 'Ayam kampung 1 ekor berat 1 kg rasanya enak banget',
-    userAvatar: 'https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-57.png'
+    userAvatar: 'https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@843c257eb10ec413f439b015bf51b4833b6e1fe4/uploads/2026-02-11T14-02-47-007Z-twl21bx45.jpg'
   }
 ]
 
