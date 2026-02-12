e2454c0b0d804cbc4780aea34a57f256f2120f32
bahasacoder <webcodebahasa@gmail.com>
2026-02-12 12:54:39 +0800

    beranda 1254

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        port: '', // Port is optional, use '' for default ports
+        pathname: '/gh/**', // Allows any path under /account123/
+      },
+     
+    ],
+  },
 };
 
 export default nextConfig;
