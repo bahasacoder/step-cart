d21e0b32f8c475f328f8de42b8c57837399e643e
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-12 09:45:02 +0800

    Update footer-panel.tsx 0944
    Update footer-panel.tsx 0944

--- components/blocks/footer/footer-panel.tsx
+++ components/blocks/footer/footer-panel.tsx
@@ -82,12 +82,15 @@ export default function Footer() {
     <div data-orientation="horizontal" data-slot="separator" className="bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px"><br /></div>
     <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-between gap-3 px-4 py-6 sm:px-6">
         <p className="font-medium">&copy;2026 <a href="#" className="link-animated">PasarBone.com</a>, Made with ❤️ for better web.</p>
-        <div className="flex flex-wrap items-center gap-4"><span data-slot="badge" className="focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-full border px-2 py-0.5 whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] [&>svg]:pointer-events-none [&>svg]:size-3 text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground text-base font-normal">
+        <div className="flex flex-wrap items-center gap-4">
+            <span data-slot="badge" className="focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-full border px-2 py-0.5 whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] [&>svg]:pointer-events-none [&>svg]:size-3 text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground text-base font-normal">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" className="lucide lucide-shield-check !size-4.5 text-green-600" aria-hidden="true">
                     <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"></path>
                     <path d="m9 12 2 2 4-4"></path>
-                </svg> Secure Payment</span>
-                <NomorHapeKu />
+                </svg> Secure Payment
+            </span>
+            <span><NomorHapeKu /></span>
+                
           </div>
     </div>
 </footer>
