b48dde4f964de18242ded117aafc280e25126d7a
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-13 21:27:30 +0800

    Create page.tsx

---
+++ app/beranda/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import HeroPage from "@/components/blocks/hero/hero-page";
+import MarqueeCategories from "@/components/blocks/marquee-categories/marquee-categories";
+import ProductHome from "@/components/blocks/product/product-home";
+import FooterPanel from "@/components/blocks/footer/footer-panel";
+
+export default function Beranda() {
+  return (
+    <>
+     <HeroPage />
+     <MarqueeCategories />
+     <ProductHome />
+     <FooterPanel />
+
+    </>
+  );
+}
