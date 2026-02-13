33ea42d5f3e9b0731088dd465eb8b7aba4061d32
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-13 21:17:17 +0800

    Delete app directory

--- app/carts/page.tsx
+++
@@ -1,6 +0,0 @@
-import Carts from "@/components/carts"
-export default function CartsPage() {
-  return (
-    <Carts />
-  )
-}
--- app/products/page.tsx
+++
@@ -1,6 +0,0 @@
-import Products from "@/components/products"
-export default function ProductsPage() {
-  return (
-    <Products />
-  )
-}
