afc2b31843c8adbd5b5faea2836f93b625df2fa7
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-13 21:50:16 +0800

    Create page.tsx

---
+++ app/fakecart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CartShop from "@/components/cart-list"
+
+export default function FakeCartPage() {
+  return (
+    <div>
+        <CartShop />
+
+    </div>
+  )
+}
