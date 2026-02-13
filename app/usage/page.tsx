9438fb31592d7f5a6aad02367fd89db253583cb5
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-13 21:59:58 +0800

    Create page.tsx shop

---
+++ app/usage/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+import { store, RootState } from '@/lib/store';
+import Shop from '@/components/shop';
+
+const Usage = () => {
+  return (
+      <Shop />
+  );
+};
+
+export default Usage;
