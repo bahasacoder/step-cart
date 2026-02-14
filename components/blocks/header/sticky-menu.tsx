70433af1cc9cf848754ec1a9080c4da6f834b236
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-14 11:22:51 +0800

    Update sticky-menu.tsx menu

--- components/blocks/header/sticky-menu.tsx
+++ components/blocks/header/sticky-menu.tsx
@@ -1,1 +1,39 @@
-/** ----- */
+import HeaderMenu from '@/components/blocks/hero/header-menu'
+import type { NavigationSection } from '@/components/blocks/menu-navigation'
+
+const navigationData: NavigationSection[] = [
+  {
+    title: 'About Us',
+    href: '/about'
+  },
+  {
+    title: 'Products',
+    href: '#'
+  },
+  {
+    title: 'Contact us',
+    href: '/contact'
+  },
+  {
+    title: 'Recipes',
+    href: '#'
+  },
+  {
+    title: 'Today\'s',
+    href: '#'
+  }
+]
+
+
+
+const HeroPage = () => {
+  return (
+    <div className='overflow-x-hidden'>
+      {/* Header Section */}
+      <HeaderMenu navigationData={navigationData} />
+
+    </div>
+  )
+}
+
+export default HeroPage
