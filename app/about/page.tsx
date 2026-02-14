156c3830db3149df5d6642e250c5e430df866ff2
bahasacoder <webcodebahasa@gmail.com>
2026-02-14 22:43:01 +0800

    page p- 2242

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,9 @@
 import AboutPage from "@/components/pages/about";
 
 export default function Page() {
-  return <AboutPage />
+  return (
+    <div className="w-full">
+      <AboutPage />
+    </div>
+  );
 }
