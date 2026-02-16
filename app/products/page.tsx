1761ef896afa268c4847af9d146f472cae092657
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-16 13:59:38 +0800

    Update page.tsx
    ProductItemsGrid

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,9 @@
 // import ShopAll from "@/components/pages/shop-all"
-import ProductPage from "@/components/pages/products"
+// import ProductPage from "@/components/pages/products"
+import ProductItemsGrid from "@/components/pages/product-items"
 
 export default function ProductsPage() {
   return (
-    <ProductPage />
+    <ProductItemsGrid />
   )
 }
