fd732755608800c5d3c0e2790356d25934833830
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-23 14:53:24 +0800

    Update page.tsx 1453

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,21 +1,11 @@
 // app/blog/[slug]/page.tsx
-export default function Page({ params }: { params: { slug: string } }) {
-  // Fetch data for the specific post using params.slug
-  // const postData = await getPostDetails(params.slug); 
+export default function Page() {
 
   return (
     <div>
       <h1>Welcome to my blog</h1>
-      <h2>Post Slug: {params.slug}</h2>
-      {/* Render the rest of your post content here */}
+    
     </div>
   );
 }
 
-// Optional: you can use generateStaticParams to pre-render static routes at build time.
-// export async function generateStaticParams() {
-//   const posts = [{ slug: 'post-1' }, { slug: 'post-2' }];
-//   return posts.map((post) => ({
-//     slug: post.slug,
-//   }));
-// }
