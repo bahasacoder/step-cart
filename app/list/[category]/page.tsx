8a75f79d994dc622bb7be91f9651429b239ca37d
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-25 14:18:57 +0800

    Update page.tsx

--- app/list/[category]/page.tsx
+++ app/list/[category]/page.tsx
@@ -1,50 +1,23 @@
-"use client"
-import React, { useEffect, useState } from "react";
-
 // app/products/[category]/page.tsx (or .js)
+
 // Define and export the generateStaticParams function
 export async function generateStaticParams() {
   // You can fetch all possible categories from an API, database, or a local source
   // const categories = await fetch('https://api.example.com/categories').then((res) => res.json());
-     interface ProductWithId {
-    id: string | number; // Use the actual type of 'id'
-    image: string;
-    title: string;
-    price: number;
-    // Add other properties as needed
-  }
-    const [products, setProducts] = useState<ProductWithId[]>([]); // from fetchdata
-    // Initialize state as an empty array []
-    useEffect(() => {
-      
-      const fetchData = async () => {
-          try {
-            axios.get('https://fakestoreapi.com/products')
-              .then(response => {
-                //console.log(response.data);
-                setProducts(response.data);
-              });
-                  
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        };
-
-      fetchData()
-    }, []); 
+  const categories = ['food', 'drink', 'meat'];
 
   // Return an array of objects, where each object matches the dynamic segment name
-  return products.map((product) => ({
-    product: product.category, // The value for the [category] dynamic segment
+  return categories.map((category) => ({
+    category: category.slug, // The value for the [category] dynamic segment
   }));
 }
 
 // Your main page component
-export default function Page({ params }: { params: { product: string } }) {
+export default function Page({ params }: { params: { category: string } }) {
   // Access the params.category here to render content for the specific category
   return (
     <div>
-      <h1>Category: {params.product}</h1>
+      <h1>Category: {params.category}</h1>
       {/* ... rest of your page content */}
     </div>
   );
