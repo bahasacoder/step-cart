a7b059a4c2441ce3a2b29480b3e928ef2057eccd
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-02-27 15:05:52 +0800

    Update page.tsx

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,26 +1,13 @@
 // app/products/[category]/page.tsx (or .js)
 import products from "@/db/products.json";
 
-// Define and export the generateStaticParams function
-export async function generateStaticParams() {
-  // You can fetch all possible categories from an API, database, or a local source
-  // const categories = await fetch('https://api.example.com/categories').then((res) => res.json());
 
-  // Return an array of objects, where each object matches the dynamic segment name
-  return products.map((product) => ({
-    category: product.category, // The value for the [category] dynamic segment
-  }));
-}
-
-// Your main page component
-export default function Page({ params }: { params: { category: string } }) {
-  // Access the params.category here to render content for the specific category
-  const { category } = params;
+export default function Page() {
+ 
   return (
     <div>
-      <h1>Category: {params.category}</h1>
-      {/* ... rest of your page content */}
-      <h1>Slug: {params}</h1>
+      <h1>Category: </h1>
+  
     </div>
   );
 }
