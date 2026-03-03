d8abc381812e994c75520913d4d024c57d8776a1
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-03 13:56:41 +0800

    Add ProductPage component for dynamic product display

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default async function ProductPage(
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex flex-col items-center gap-4">
+         <p>Product Page</p>
+      </div>
+    </div>
+  );
+}
