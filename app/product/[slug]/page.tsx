803d39aa051d03d307584fa60e387b24b5840038
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-24 21:05:13 +0800

    Implement product detail page with static params

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,1 +1,36 @@
 
+// import { fetchItems } from "@/actions/getDb"
+import products from "@/db/products.json";
+
+export async function generateStaticParams() {
+  // Fetch your data to get the list of IDs
+  //const posts = await fetch('https://.../items').then((res) => res.json());
+
+  // Return an array of IDs to be pre-rendered
+  return products.map((product) => ({
+    slug: product.slug.toString(), // id must be a string
+    id: product.id.toString()
+  }));
+}
+
+export default async function ItemDetailPage(
+  { params }: { params: { slug: string } }
+)  {
+  const { slug, id } = await params
+  const found = await products.find((item) => product.id === id );
+
+  return (
+
+
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex flex-col items-center gap-4">
+         <p>Product Page</p>
+      </div>
+      <div> 
+        <h2>id products : { id }</h2>
+         <h2>slug products : { slug }</h2>
+      </div>
+    </div>
+  );
+
+}
