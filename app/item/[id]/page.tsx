36dcdf8b7cb29b94793bd873e7f1ba4b244b0b9c
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-24 14:51:15 +0800

    Update page.tsx

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -1,17 +1,35 @@
-// import get_item from "@/actions/getDb"
+// import { fetchItems } from "@/actions/getDb"
+import items from "@/db/items.json";
+/*
 export async function generateStaticParams() {
   return [{ id: "10001" }, { id : "10002"}, { id : "10003"}, { id : "10004"}, { id : "10005"}];
 }
+  */
 
-export default async function ProductPage(
-  { params }: { params: Promise<{ id: string }> }
-) {
-  const { id } = await params;
+
+export default async function ItemDetailPage(
+  { params }: { params: { id: string } }
+)  {
+  const { id } = await params
+  const found = await items.find((item) => item.id === id );
+
   return (
+
+
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-col items-center gap-4">
-         <p>Product Page {id}</p>
+         <p>Product Page</p>
+      </div>
+      <div>
+        {items.map((item) => (
+          <p key={item.id}>{item.value}</p>
+        ))}
+      </div>
+      <div>
+        <h2>found : { found?.value }</h2>
+        <h2>iditem : { id }</h2>
       </div>
     </div>
   );
+
 }
