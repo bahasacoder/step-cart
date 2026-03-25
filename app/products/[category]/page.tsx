375698d8f9d7ba4dd06a173dd8d2f549c11626be
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-25 11:14:52 +0800

    Update page.tsx category

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCategoryList from "@/componetns/blocks/product-category-list";
 export async function generateStaticParams() {
   return [{ "category" : "meats" }, { "category" : "fruits"}, { "category" : "drinks"}];
 }
@@ -7,11 +8,14 @@ export default async function CategoryPage(
 ) {
   const { category } = await params;
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex flex-col items-center gap-4">
-         <p>Product Category</p>
-        <h3>{ category }</h3>
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <div className="flex flex-col items-center gap-4">
+           <p>Product Category</p>
+          <h3>{ category }</h3>
+        </div>
       </div>
-    </div>
+      <ProductCategoryList />
+    </>
   );
 }
