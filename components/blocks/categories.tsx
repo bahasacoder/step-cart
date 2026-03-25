ba85bcef4c9513cbea4f7b90407324f4f9ce5350
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-25 10:15:43 +0800

    Update categories.tsx style=

--- components/blocks/categories.tsx
+++ components/blocks/categories.tsx
@@ -9,7 +9,7 @@ export default function CategoriesBlock() {
             </div>
             <div className="relative w-full" role="region" aria-roledescription="carousel" data-slot="carousel">
                 <div className="overflow-hidden" data-slot="carousel-content">
-                    <div className="flex -ml-6" style="transform: translate3d(0px, 0px, 0px);">
+                    <div className="flex -ml-6" style={{transform: "translate3d(0px, 0px, 0px)"}}>
                         <div role="group" aria-roledescription="slide" data-slot="carousel-item" className="min-w-0 shrink-0 grow-0 basis-full pl-6 md:basis-1/3 lg:basis-1/6"><a href="#">
                                 <div className="bg-muted mb-6 flex h-32 items-center justify-center rounded-md px-11.25"><img src="https://cdn.shadcnstudio.com/ss-assets/blocks/ecommerce/product-category/image-10.png" alt="Smart Electronics" className="h-22 object-contain"></div>
                                 <h2 className="text-center text-lg font-medium">Smart Electronics</h2>
