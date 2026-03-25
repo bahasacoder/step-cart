57f62fce07ce2f8734d34e96c9ec9bce4ed0c696
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-25 09:28:51 +0800

    Update page category.tsx
    category

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,5 +1,5 @@
 export async function generateStaticParams() {
-  return [{ "namecat" : "meats" }, { "namecat" : "fruits"}, { "namecat" : "drinks"}];
+  return [{ "category" : "meats" }, { "category" : "fruits"}, { "category" : "drinks"}];
 }
 
 export default async function CategoryPage(
@@ -9,7 +9,8 @@ export default async function CategoryPage(
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-col items-center gap-4">
-         <p>Product Page</p>
+         <p>Product Category</p>
+        <h3>{ category }</h3>
       </div>
     </div>
   );
