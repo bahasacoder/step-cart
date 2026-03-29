8d68caa62b0e1c75166265485a2ce067a400ab97
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-29 22:54:08 +0800

    Refactor CategoryPage by removing unused code
    Removed unused imports and the getUniqueCategories function. Simplified the CategoryPage component.

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,26 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import products from "@/db/products.json";
 import ProductListDelapan from "@/components/blocks/product-list-delapan";
 
-function getUniqueCategories(categs:any) {
-  if (!Array.isArray(categs)) {
-    throw new Error("Input must be an array");
-  }
-
-  return [...new Set(
-    categs
-      .filter(item => item && typeof item.category === "string") // validate
-      .map(item => item.category.trim()) // normalize
-  )];
-}
-
-try {
-  const categories = getUniqueCategories(products);
-  console.log("try",categories); // ["Electronics", "Clothing", "Books"];
-} catch (error) {
-  console.error("Error:", (error as Error).message);
-}
 export async function generateStaticParams() {
   return [{ "category" : "meats" }, { "category" : "fruits"}, { "category" : "drinks"}];
 }
@@ -28,12 +7,9 @@ export async function generateStaticParams() {
 export default async function CategoryPage(
   { params }: { params: Promise<{ category: string }> }
 ) {
-   const categories = getUniqueCategories(products);
-   console.log("categ",categories);
   const { category } = await params;
   return (
     <>
-     
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <div className="flex flex-col items-center gap-4">
            <p>Product Category</p>
