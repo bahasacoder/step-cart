906fca5a1e901a8417be59767a6a260aa88bc6af
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-03-30 22:27:21 +0800

    Remove category display from product page 2227
    Removed category display from the product page.

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -34,12 +34,7 @@ export default async function CategoryPage(
   return (
     <>
      
-      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <div className="flex flex-col items-center gap-4">
-           <p>Product Category</p>
-          <h3>{ category }</h3>
-        </div>
-      </div>
+    
       <ProductListDelapan />
     </>
   );
