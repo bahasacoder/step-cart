a399dd11d7d702a227de092c51a6aa548d2e6eab
bahasacoder <webcodebahasa@gmail.com>
2026-04-10 23:03:49 +0800

    posts 23;03

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,1 +1,26 @@
-export default function Posts() {}
+"use client"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import posts from "@/db/posts.json";
+
+export default function PostsPage() {
+    const [results, setResults] = useState([]);
+    const getResults = async () => {
+        await axios
+            .get('http://')
+            .then(response => {
+                setResults(response.data.MRData)
+            })
+    }
+    useEffect(()=>{
+        getResults()
+    }, [])
+
+    return (
+        <>
+            <div>
+                <h1>Posts</h1>
+            </div>
+        </>
+    )
+}
