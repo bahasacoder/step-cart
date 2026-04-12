4bc642d90f80245efe10332d026ffd9b3eaaa83e
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-04-12 22:27:58 +0800

    Remove post ID display from posts list

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -14,7 +14,6 @@ export default function PostsPage() {
             <div>
                 {posts.map((post) => (
                     <div key={post.id}>
-                        <p>{post.id}</p>
                         <h2>{post.title}</h2>       
                         <p>{post.content}</p>
                     </div>
