f944a4b9dcda4be2fe3d4cb3b1d774d6462389f9
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-04-13 09:31:53 +0800

    Implement static params generation for edit page

--- app/posts/edit/[id]/page.tsx
+++ app/posts/edit/[id]/page.tsx
@@ -1,1 +1,21 @@
-// page form edit
+import posts from "@/db/posts.json";
+// Return a list of `params` to populate the [slug] dynamic segment
+export async function generateStaticParams() {
+  // const posts = await fetch('https://.../posts').then((res) => res.json())
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
+export default async function DetailEditPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  return (
+    <>
+      <h1>Halaman Detail Edit Page</h1>
+    </>
+  )
+}
