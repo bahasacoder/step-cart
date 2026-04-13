0a7b7bc3032efea53962831030504d62169012d9
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-04-13 09:32:17 +0800

    Change slug to id in generateStaticParams

--- app/posts/edit/[id]/page.tsx
+++ app/posts/edit/[id]/page.tsx
@@ -3,7 +3,7 @@ import posts from "@/db/posts.json";
 export async function generateStaticParams() {
   // const posts = await fetch('https://.../posts').then((res) => res.json())
   return posts.map((post) => ({
-    slug: post.slug,
+    id: post.id,
   }))
 }
 
