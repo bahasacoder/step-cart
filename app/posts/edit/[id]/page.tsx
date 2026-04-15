ebdfa3d1c60e58b2387862ec15bac3f2a87cd73f
bahasacoder <webcodebahasa@gmail.com>
2026-04-15 21:40:40 +0800

    form edit id 2140

--- app/posts/edit/[id]/page.tsx
+++ app/posts/edit/[id]/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import posts from "@/db/posts.json";
 import FormEditPost from "./form-post";
 // Return a list of `params` to populate the [slug] dynamic segment
@@ -21,27 +20,3 @@ export default async function DetailEditPage({
     </>
   )
 }
-=======
-import posts from "@/db/posts.json";
-import FormEditPost from "./form-post";
-// Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams() {
-  // const posts = await fetch('https://.../posts').then((res) => res.json())
-  return posts.map((post) => ({
-    id: post.id,
-  }))
-}
-
-export default async function DetailEditPage({
-  params,
-}: {
-  params: Promise<{ id: string }>
-}) {
-  const { id } = await params
-  return (
-    <>
-      <FormEditPost id={id} />
-    </>
-  )
-}
->>>>>>> ad606bca612e00dec0b864e514b841c40303963d
--- app/posts/edit/[id]/pageasli.tsx
+++ app/posts/edit/[id]/pageasli.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import posts from "@/db/posts.json";
 // Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
@@ -21,27 +20,3 @@ export default async function DetailEditPageAsli({
     </>
   )
 }
-=======
-import posts from "@/db/posts.json";
-// Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams() {
-  // const posts = await fetch('https://.../posts').then((res) => res.json())
-  return posts.map((post) => ({
-    id: post.id,
-  }))
-}
-
-export default async function DetailEditPageAsli({
-  params,
-}: {
-  params: Promise<{ id: string }>
-}) {
-  const { id } = await params
-  return (
-    <>
-      <h1>Halaman Detail Edit Page</h1>
-      <p> id: {id} </p>
-    </>
-  )
-}
->>>>>>> ad606bca612e00dec0b864e514b841c40303963d
