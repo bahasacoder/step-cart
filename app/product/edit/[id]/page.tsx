b08f574520f7252b3c4c83ef5dc2fb3793837b46
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-05-13 22:01:49 +0800

    Update generateStaticParams to fetch item IDs
    Refactor static params generation to use items from JSON.

--- app/product/edit/[id]/page.tsx
+++ app/product/edit/[id]/page.tsx
@@ -2,11 +2,19 @@
 
 import { useState } from "react";
 
+import items from "@/db/items.json";
 export async function generateStaticParams() {
-  const product = [{}];
-  return  product;
-} 
-export default function HomePage() {
+  // Fetch your data to get the list of IDs
+  //const posts = await fetch('https://.../items').then((res) => res.json());
+
+  // Return an array of IDs to be pre-rendered
+  return items.map((item) => ({
+    id: item.id.toString(), // id must be a string
+  }));
+}
+
+
+export default function EditProductPage() {
   
   return (
     <main style={{ padding: "20px", fontFamily: "sans-serif" }}>
