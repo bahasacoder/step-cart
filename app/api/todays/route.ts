42c4a73e9438cb2bd507f7c016f93659a5286923
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-05-14 22:35:26 +0800

    Implement GET endpoint to read todays.json

--- app/api/todays/route.ts
+++ app/api/todays/route.ts
@@ -1,1 +1,10 @@
-//
+import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+import path from "path";
+
+export async function GET() { 
+  const filePath = path.join(process.cwd(), "db", "todays.json");
+  const fileData = await fs.readFile(filePath, "utf8");
+  const users = JSON.parse(fileData);
+  return Response.json(users);  
+}
