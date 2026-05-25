5ce8341fcf1900ebc2dfc73bbfd7ada2231b4b5d
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-05-25 13:58:24 +0800

    Update route.ts

--- app/api/todays/[id]/route.ts
+++ app/api/todays/[id]/route.ts
@@ -52,38 +52,3 @@ export async function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 }
-
-export async function PATCH(request: Request,   { params }: { params: Promise<{ id: string }>} ) {
-  const { id, name, email } = await request.json();
-  const updatedUser = { id, name, email };
-  let idUpdated = updatedUser.id;
-  let nameUpdated = updatedUser.name;
-  let emailUpdated = updatedUser.email;
-
-  console.log("PUT request received for user ID:", id);
-  console.log("Updateuser Id:", updatedUser);
-
-  const filePath = path.join(process.cwd(), "data", "users.json");
-  const fileData = await fs.readFile(filePath, "utf8");
-  let users = JSON.parse(fileData);
-  //console.log("users from jsonfile:", users);
-  // bagaimana memasukkan updatedUser ke dalam users array dengan id yang sama?  
-  /* users = users.map((user: any) => (user.id === updatedUser.id ? updatedUser : user)); */
-  const targetId = id;
-  const updates = { name: nameUpdated, email: emailUpdated };
-  // Find index of the object with matching id
-  const index = users.findIndex((user: any) => user.id === parseInt(targetId));
-  console.log("Index of user to update:", index);
-  if (index !== -1) {
-    // Update only specific fields
-    Object.assign(users[index], updates);
-  } else {
-    console.log(`User with id ${targetId} not found.`);
-  }
-  console.log("users array after update attempt:", users);
-
-  console.log("updatedUser object:", updatedUser);
-  await fs.writeFile(filePath, JSON.stringify(users, null, 2));
-  return NextResponse.json({ message: "User updated", user: updatedUser });
-  
-}   
