f193d3d818be8c5380fc6664535110e7a57a3536
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-05-26 14:34:13 +0800

    Refactor EditTodayPage by removing unused code
    Removed the ItemsTodays interface and the generateStaticParams function, simplifying the EditTodayPage component.

--- app/todays/edit/[id]/page.tsx
+++ app/todays/edit/[id]/page.tsx
@@ -1,36 +1,9 @@
 
-interface ItemsTodays {
-  nama: string;
-  harga: string;  
-  kategori: string;
-  deskripsi: string;
-  lokasi: string;
-  gambar1: string;  
-  gambar2: string;
-  gambar3: string;
-  map: string;
-  penjual: string;
-  kontak: string;
-  alamat: string;
-  id: string;
-}
 
 
-export async function generateStaticParams() {
-  const response = await fetch("http://localhost:3000/api/todays", {
-     method: "GET",
-     headers: {
-       "Content-Type": "application/json",
-     },
-   }
-  );
-  const itemsTodays: ItemsTodays[] = await response.json();  
-  return itemsTodays.map(itemTodays => ({ id: itemTodays.id.toString() }));  
-}
 
+export default async function EditTodayPage(){
 
-export default async function EditTodayPage({ params }: { params: Promise<{ id: string } > }){
-  const { id } = await params;
 
   return (
     <>
