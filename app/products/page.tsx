eaba5a18369ccd3eb8da15916b58dbbac2da2635
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-12 13:23:27 +0800

    Import and use ProductItemsGrid in ProductsPage

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,13 @@
 // import ShopAll from "@/components/pages/shop-all"
 // import ProductPage from "@/components/pages/products"
-//import ProductItemsGrid from "@/components/pages/product-items-grid"
+import ProductItemsGrid from "@/components/pages/product-items-grid"
 import CategoriesBlock from "@/components/blocks/categories"
 
 export default function ProductsPage() {
   return (
     <>
       <CategoriesBlock />
+      <ProductItemsGrid />
     </>
   )
 }
