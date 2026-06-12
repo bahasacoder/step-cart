c38be3b9921ace3c735f5c2bd1bdf8bd2a7b4476
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-12 11:42:25 +0800

    Update page.tsx

--- app/todays/page.tsx
+++ app/todays/page.tsx
@@ -1,5 +1,23 @@
-export default function HomeTodayPage(){
-  return (
-    <div>This Home Today's</div>
-  )
+export default async function ItemsTodaysPage() {
+    const response = await fetch(`http://localhost:3000/api/todays`, {
+        method: "GET"
+        }
+      );
+      const itemsTodays = await response.json();
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <div className="flex flex-col items-center gap-4">
+                <p>Items Todays Page</p>    
+            </div>
+            <div>
+            {
+                itemsTodays.map((item: any) => (
+                    <p key={item.id}>{item.nama}</p>
+                ))
+            }
+            </div>
+        </div>
+
+    )
 }
