603bae11d4915c9529b3b5184aaa6fc230225175
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-13 21:50:16 +0800

    Update page.tsx

--- app/todays/edit/[id]/page.tsx
+++ app/todays/edit/[id]/page.tsx
@@ -20,8 +20,8 @@ export default async function EditItemTodaysPage(
   { params }: EditItemTodaysProps
 )  {
   const { id } = await params;
- // const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://pasarbone.com';
-  const response = await fetch(`/api/todays`, {
+ const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://pasarbone.com';
+  const response = await fetch(`${baseUrl}/api/todays`, {
       method: "GET"
       }
     );
