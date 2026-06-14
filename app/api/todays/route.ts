dbbf02520499944e8ecf09d127aad2105eb81739
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-14 23:16:58 +0800

    Update GET function to use remote JSON file
    Change file path to fetch JSON data from a remote URL.

--- app/api/todays/route.ts
+++ app/api/todays/route.ts
@@ -4,7 +4,8 @@ import { promises as fs } from "fs";
 import path from "path";
 
 export async function GET() { 
-  const filePath = path.join(process.cwd(), "db", "todays.json");
+  // const filePath = path.join(process.cwd(), "db", "todays.json");
+  const filePath = `https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json`;
   const fileData = await fs.readFile(filePath, "utf8");
   const users = JSON.parse(fileData);
   return Response.json(users);  
