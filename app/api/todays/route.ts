7c179f1f2a48dff7bea7b266745d1224fc6abcf6
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-15 10:41:13 +0800

    Refactor filePath construction in GET function
    Updated filePath to use path.join for constructing the URL.

--- app/api/todays/route.ts
+++ app/api/todays/route.ts
@@ -4,8 +4,9 @@ import { promises as fs } from "fs";
 import path from "path";
 
 export async function GET() { 
+  // https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json`
   // const filePath = path.join(process.cwd(), "db", "todays.json");
-  const filePath = `https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json`;
+  const filePath = path.join(process.cwd(), `https://raw.githubusercontent.com/bahasacoder/step-cart/main/db`, `todays.json`;
   const fileData = await fs.readFile(filePath, "utf8");
   const users = JSON.parse(fileData);
   return Response.json(users);  
