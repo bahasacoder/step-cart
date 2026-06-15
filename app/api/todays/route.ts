da80c00a86ece67fa380b8f22a170d1d3cfc2561
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-15 10:48:38 +0800

    Fix syntax error in file path definition

--- app/api/todays/route.ts
+++ app/api/todays/route.ts
@@ -6,7 +6,7 @@ import path from "path";
 export async function GET() { 
   // https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json`
   // const filePath = path.join(process.cwd(), "db", "todays.json");
-  const filePath = path.join(process.cwd(), `main/db`, `todays.json`;
+  const filePath = path.join(process.cwd(), `main/db`, `todays.json`);
   const fileData = await fs.readFile(filePath, "utf8");
   const users = JSON.parse(fileData);
   return Response.json(users);  
