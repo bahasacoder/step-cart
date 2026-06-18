1f5e9c910db1fce499577e0df0c64ac5bfb9c4b5
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-18 14:46:04 +0800

    Rename handleSubmitItem and add myActionItems function
    Renamed handleSubmitItem to handleSubmitUser and added myActionItems function to handle form submissions.

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,7 @@
 // app/actions.ts
 'use server' // Marks this file's functions to run exclusively on the server
 
-export async function handleSubmitItem(formData: FormData) {
+export async function handleSubmitUser(formData: FormData) {
   // Extract inputs using their native HTML 'name' attribute
   const email = formData.get('email') as string
   const password = formData.get('password') as string
@@ -9,3 +9,32 @@ export async function handleSubmitItem(formData: FormData) {
   // Process data (e.g., save to a database or authenticate)
   console.log({ email, password })
 }
+
+export async function myActionItems(formData: FormData): Promise<{ success: boolean }> {
+        "use server";
+        // Logic to handle form submission and add item to the database
+        console.log("Item added!");
+        const rawFormData = {
+                Value: formData.get('value'),
+                Trigger: formData.get('trigger'),
+                Content: formData.get('content'),
+        }
+        console.log("Raw Form Data:", rawFormData);
+            const filePath = path.join(process.cwd(), "db", "todays.json");
+            const fileData = await fs.readFile(filePath, "utf8");
+           //  const itemsTodays = JSON.parse(fileData);
+           const itemsTodays = [];
+            itemsTodays.push(rawFormData);
+            // await fs.writeFile(filePath, JSON.stringify(itemsTodays, null, 2));
+            console.log("Updated Items:", itemsTodays);
+        
+         try {
+                await fs.writeFile(filePath, JSON.stringify(itemsTodays, null, 2), 'utf-8');
+                return { success: true };
+            } catch (error) {
+                console.error('Failed to write file:', error);
+                return { success: false };
+            }
+       
+    }
+
