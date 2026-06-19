7ee13c007b723c51e73603301e5d9ce3982f55b0
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-19 16:30:09 +0800

    Replace fetch with local TodaysDB for item retrieval
    Refactor to use local TodaysDB instead of fetching data.

--- app/todays/edit/[id]/page.tsx
+++ app/todays/edit/[id]/page.tsx
@@ -1,7 +1,9 @@
+import TodaysDB from '@/db/todays.json';
 import EditItemTodaysForm from "./edit-item-todays-form"; 
 export async function generateStaticParams() {
   //const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://pasarbone.com';
     // "https://raw.githubusercontent.com/USERNAME/REPO/BRANCH/path/to/bundledb.json";
+  /*
  const raw_todays = "https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json"
   const response = await fetch(`https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json`, {
      method: "GET",
@@ -12,6 +14,8 @@ export async function generateStaticParams() {
    }
   );
   const itemsTodays = await response.json();  
+  */
+  const itemsTodays = await TodaysDB;
   return itemsTodays.map((item: any) => ({ id: item.id.toString() }));  
 }
 
@@ -24,13 +28,15 @@ export default async function EditItemTodaysPage(
   const { id } = await params;
   // const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://pasarbone.com';
   // https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json
+  /*
    const raw_todays = "https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json"
-
   const response = await fetch(`https://raw.githubusercontent.com/bahasacoder/step-cart/main/db/todays.json`, {
       method: "GET"
       }
     );
     const itemsTodays = await response.json();
+    */
+  const itemsTodays = await TodaysDB;
   //const mapItemsTodays = itemsTodays.map((ait: any) => ait.id === id ? ait : null);
   const findItemTodays = itemsTodays.find((ait: any) => ait.id ===  parseInt(id));
         console.log("found ", findItemTodays);
