e4c1299ca3800c5736a177e418dafa32712fc8cf
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-09 11:05:10 +0800

    Add PopoverHome component to ListItems

--- app/item/page.tsx
+++ app/item/page.tsx
@@ -1,10 +1,11 @@
 import Items from "@/db/items.json";
 import path from "path";
 import fs from "fs/promises";
-
+import PopoverHome from "@/components/popover-home"
 export default async function ListItems() {   
     
     return (
+        <><PopoverHome /></>
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <div className="flex flex-col items-center gap-4">
                 <p>Item List Page</p>
