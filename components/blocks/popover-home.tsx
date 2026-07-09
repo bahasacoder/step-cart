c00a988ca78004c94d815695f8ea0049ebbdcf7f
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-09 16:23:44 +0800

    Update popover-home.tsx

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -1,7 +1,8 @@
 export default function PopoverHome() {
+  // absolute
   return(
     <div data-popover="popover"
-      className="absolute p-4 font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
+      className="flex mx-auto my-auto p-4 h-24 w-24 font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
       Kami Juga Melayani Pembuatan Website UMKM
     </div> 
     
