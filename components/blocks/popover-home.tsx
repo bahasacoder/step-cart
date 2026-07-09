3ac2cea21b377bb8ff7f0ffd737ab8f9adec9d6a
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-09 21:11:14 +0800

    Update DialogTrigger to use new button text

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -15,7 +15,9 @@ export default function PopoverHome() {
       className="flex items-center justify-center mx-auto my-auto p-4 h-24 w-24 font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
      
        <Dialog>
-          <DialogTrigger render={<Button variant="outline">Share</Button>} />
+          <DialogTrigger>
+            <Button>Open Dialog</Button>
+          </DialogTrigger>
           <DialogContent className="sm:max-w-md">
             
              <DialogTitle>Are you absolutely sure?</DialogTitle>
