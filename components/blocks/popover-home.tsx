b230b344978362ae1f8a173fc67f0c6fd9fa4bce
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-09 19:16:54 +0800

    Add dialog functionality to PopoverHome component

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 export default function PopoverHome() {
   // absolute
   return(
     <div data-popover="popover"
       className="flex items-center justify-center mx-auto my-auto p-4 h-24 w-24 font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
-      Kami Juga Melayani Pembuatan Website UMKM
+     
+       <Dialog>
+          <DialogTrigger render={<Button variant="outline">Share</Button>} />
+          <DialogContent className="sm:max-w-md">
+            
+             <DialogTitle>Are you absolutely sure?</DialogTitle>
+               <h1 className="text-2x text-center">Kami Juga Melayani Pembuatan Website UMKM</h1>
+          </DialogContent>
+        </Dialog>
+      
     </div> 
     
   )
