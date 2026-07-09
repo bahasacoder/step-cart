21c32c538c45c07d2fa8d6a395cd0d9c706c5635
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-09 21:14:21 +0800

    Update popover-home.tsx

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -12,7 +12,7 @@ export default function PopoverHome() {
   // absolute
   return(
     <div data-popover="popover"
-      className="flex items-center justify-center mx-auto my-auto p-4 h-24 w-24 font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
+      className="flex items-center justify-center mx-auto my-auto p-4 h-24 w-24 border-2 border-red font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
      
        <Dialog>
           <DialogTrigger>
