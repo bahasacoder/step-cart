237d3fa7e85bcdc0d42a80b9ba2ba55ae49fc5d8
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-09 11:07:49 +0800

    Update page.tsx

--- app/item/page.tsx
+++ app/item/page.tsx
@@ -1,23 +1,25 @@
 import Items from "@/db/items.json";
 import path from "path";
 import fs from "fs/promises";
-import PopoverHome from "@/components/popover-home"
+import PopoverHome from "@/components/popover-home";
 export default async function ListItems() {   
     
     return (
-        <><PopoverHome /></>
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <div className="flex flex-col items-center gap-4">
-                <p>Item List Page</p>
+        <>
+            <PopoverHome />
+            <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+                <div className="flex flex-col items-center gap-4">
+                    <p>Item List Page</p>
+                </div>
+                <div>
+                {
+                    Items.map((item) => (
+                        <p key={item.id}>{item.value}</p>
+                    ))
+                }
+                </div>
             </div>
-            <div>
-            {
-                Items.map((item) => (
-                    <p key={item.id}>{item.value}</p>
-                ))
-            }
-            </div>
-        </div>
+        </>
     )
     
 }
