be3c7c097722fe98eb6a8a398c6f2937f70a5eb5
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-10 09:50:24 +0800

    Refactor PopoverHome to add image and adjust dialog
    Updated PopoverHome component to include an image and modified dialog structure.

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -12,16 +13,20 @@ export default function PopoverHome() {
   // absolute
   return(
     <div data-popover="popover"
-      className="flex items-center justify-center mx-auto my-auto p-4 h-24 w-24 border-2 border-red font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
+      className="flex font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
      
-       <Dialog defaultOpen="true">
-          <DialogTrigger>
-            <Button>Open Dialog</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-md">
-            
-             <DialogTitle>Are you absolutely sure?</DialogTitle>
-               <h1 className="text-2x text-center">Kami Juga Melayani Pembuatan Website UMKM</h1>
+       <Dialog defaultOpen>
+          <DialogContent className="sm:max-w-md">            
+             <DialogTitle></DialogTitle>
+              <div>
+                 <h1 className="text-2x text-center">Kami Juga Melayani Pembuatan Website UMKM</h1>
+                  <Image
+                    src="/profile.png"
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"
+                  />
+                </div>
           </DialogContent>
         </Dialog>
       
