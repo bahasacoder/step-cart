c7c5649d68ffe284df738cf46b32d9ef87e88def
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-10 10:55:54 +0800

    Update heading size in popover-home component

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -19,16 +19,8 @@ export default function PopoverHome() {
           <DialogContent className="sm:max-w-md">            
              <DialogTitle></DialogTitle>
               <div className="relative h-48 w-48 mx-auto my-auto p-4">
-                 <h1 className="absolute top-0 left-0 text-5x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:font-bold hover:tracking-widest">Kami Juga Melayani Pembuatan Website UMKM</h1>
-                  <Image
-                    src="https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@70c3f27ff5a6d467cf2db3331d5744f0431eb500/uploads/2026-02-11T14-57-41-361Z-vizvnobtw.png"
-                    width={500}
-                    height={500}
-                    alt="Web Developer"
-                    fill
-                    priority
-                    className="border-2 relative right-0"
-                  />
+                 <h1 className="absolute top-0 left-0 text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:font-bold hover:tracking-widest">Kami Juga Melayani Pembuatan Website UMKM</h1>
+                 
                 </div>
           </DialogContent>
         </Dialog>
