0d582ccbe2d370e285b5edec9d9c5955b7bf9c59
bahasacoder <webcodebahasa@gmail.com>
2026-07-10 21:35:18 +0800

    kerja dialog home

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -12,15 +12,12 @@ export default function PopoverHome() {
   // absolute
   return(
     <div data-popover="popover"
-      className="flex items-center justify-center mx-auto my-auto p-4 h-24 w-24 border-2 border-red font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
+      className="flex font-sans text-sm font-normal break-words whitespace-normal bg-white border rounded-lg shadow-lg w-max border-blue-gray-50 text-blue-gray-500 shadow-blue-gray-500/10 focus:outline-none">
      
-       <Dialog defaultOpen="true">
-          <DialogTrigger>
-            <Button>Open Dialog</Button>
-          </DialogTrigger>
+       <Dialog defaultOpen>
           <DialogContent className="sm:max-w-md">
             
-             <DialogTitle>Are you absolutely sure?</DialogTitle>
+             <DialogTitle></DialogTitle>
                <h1 className="text-2x text-center">Kami Juga Melayani Pembuatan Website UMKM</h1>
           </DialogContent>
         </Dialog>
