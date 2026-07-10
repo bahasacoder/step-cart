cdb794f1c627fc0f5bd3ffc2e7f95c75db1a1413
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-07-10 10:00:25 +0800

    Update image source and alt text in popover-home

--- components/blocks/popover-home.tsx
+++ components/blocks/popover-home.tsx
@@ -21,10 +21,12 @@ export default function PopoverHome() {
               <div>
                  <h1 className="text-2x text-center">Kami Juga Melayani Pembuatan Website UMKM</h1>
                   <Image
-                    src="/profile.png"
+                    src="https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@ed2c10a5b9c2baebab4b37575b0ef096bc343d03/uploads/2026-07-10T01-54-58-805Z-f3mdld6v0.jpg"
                     width={500}
                     height={500}
-                    alt="Picture of the author"
+                    alt="Web Developer"
+                    fill
+                    priority
                   />
                 </div>
           </DialogContent>
