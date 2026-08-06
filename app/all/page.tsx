3381871b92c0a180886658ecffe46b4394c9b435
bahasacoder <webcodebahasa@gmail.com>
2026-08-06 21:45:30 +0800

    all 8062145

--- app/all/page.tsx
+++ app/all/page.tsx
@@ -1,46 +1,64 @@
+"use client"
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 export default function AllProductsPage() {
-  
+  const [quantity, setQuantity] = useState(5);
+  const [selectedOption, setSelectedOption] = useState("0");
+
   return (
     <div>
-       <section className="module">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-6 mb-sm-40"><a className="gallery" href="assets/images/shop/product-7.jpg"><img src="assets/images/shop/product-7.jpg" alt="Single Product Image"></a>
-                <ul className="product-gallery">
-                  <li><a className="gallery" href="assets/images/shop/product-8.jpg"></a><img src="assets/images/shop/product-8.jpg" alt="Single Product"></li>
-                  <li><a className="gallery" href="assets/images/shop/product-9.jpg"></a><img src="assets/images/shop/product-9.jpg" alt="Single Product"></li>
-                  <li><a className="gallery" href="assets/images/shop/product-10.jpg"></a><img src="assets/images/shop/product-10.jpg" alt="Single Product"></li>
+       <section className="relative block p-[140px]">
+          <div className="container max-w-6xl flex flex-wrap p-0 m-auto">
+
+            <div className="row w-full m-0 flex flex-wrap justify-between items-start gap-4">
+              <div className="col-sm-6 w-full md:max-w-4/9 flex flex-col border-2 border-red-200 float">
+                <div className="product-gallery absolute top-0 left-0 h-[550px] flex mb-2 pb-4 relative bg-yellow-100 ">
+                    <Link className="gallery" href="/assets/images/shop/product-7.jpg">
+                      <Image 
+                        src="/assets/images/shop/product-7.jpg" alt="Single Product Image"
+                        fill 
+                        className="w-full h-full"
+                      />
+                    </Link>
+                </div>
+                <ul className="product-gallery w-full flex flex-row p-0 m-0 gap-2 justify-start items-center overflow-x-auto">
+                  <li><a className="gallery" href="/assets/images/shop/product-8.jpg"></a><Image src="/assets/images/shop/product-8.jpg" alt="Single Product" width={100} height={100}/></li>
+                  <li><a className="gallery" href="/assets/images/shop/product-9.jpg"></a><Image src="/assets/images/shop/product-9.jpg" alt="Single Product"  width={100} height={100}/></li>
+                  <li><a className="gallery" href="/assets/images/shop/product-10.jpg"></a><Image src="/assets/images/shop/product-10.jpg" alt="Single Product"  width={100} height={100}/></li>
                 </ul>
               </div>
-              <div className="col-sm-6">
+              <div className="col-sm-6 float-left w-full md:w-1/2 bg-orange-100 p-6">
                 <div className="row">
-                  <div className="col-sm-12">
-                    <h1 className="product-title font-alt">Accessories Pack</h1>
+                  <div className="col-sm-12 mt-0 my-8">
+                    <h1 className="product-title text-4xl">Accessories Pack</h1>
                   </div>
                 </div>
-                <div className="row mb-20">
+                <div className="row w-full my-4 ">
                   <div className="col-sm-12"><span><i className="fa fa-star star"></i></span><span><i className="fa fa-star star"></i></span><span><i className="fa fa-star star"></i></span><span><i className="fa fa-star star"></i></span><span><i className="fa fa-star star-off"></i></span><a className="open-tab section-scroll" href="#reviews">-2customer reviews</a>
                   </div>
                 </div>
-                <div className="row mb-20">
+                <div className="row w-full mt-4 mb-8">
                   <div className="col-sm-12">
-                    <div className="price font-alt"><span className="amount">£20.00</span></div>
+                    <div className="price tracking-widest uppercase text-3xl"><span className="amount">£20.00</span></div>
                   </div>
                 </div>
-                <div className="row mb-20">
+                <div className="row mb-18">
                   <div className="col-sm-12">
                     <div className="description">
                       <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.</p>
                     </div>
                   </div>
                 </div>
-                <div className="row mb-20">
-                  <div className="col-sm-4 mb-sm-20">
-                    <input className="form-control input-lg" type="number" name="" value="1" max="40" min="1" required="required">
+                <div className="row w-full flex my-2 justify-between items-center">
+                  <div className="col-sm-4 bg-rose-100 w-3xs mx-auto my-2 flex items-center justify-center">
+                    <input className="form-control border border-gray-300 px-4 py-2 w-4/5" type="number" name="" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} max="40" min="1" required />
                   </div>
-                  <div className="col-sm-8"><a className="btn btn-lg btn-block btn-round btn-b" href="#">Add To Cart</a></div>
+                  <div className="col-sm-8 bg-black w-full mx-auto my-2 flex items-center justify-center">
+                    <Link className="btn px-6 py-2 uppercase text-white" href="#">Add To Cart</Link>
+                  </div>
                 </div>
-                <div className="row mb-20">
+                <div className="row mb-18">
                   <div className="col-sm-12">
                     <div className="product_meta">Categories:<a href="#"> Man, </a><a href="#">Clothing, </a><a href="#">T-shirts</a>
                     </div>
@@ -48,6 +66,7 @@ export default function AllProductsPage() {
                 </div>
               </div>
             </div>
+
             <div className="row mt-70">
               <div className="col-sm-12">
                 <ul className="nav nav-tabs font-alt" role="tablist">
@@ -89,7 +108,7 @@ export default function AllProductsPage() {
                   <div className="tab-pane" id="reviews">
                     <div className="comments reviews">
                       <div className="comment clearfix">
-                        <div className="comment-avatar"><img src="" alt="avatar"></div>
+                        <div className="comment-avatar"><Image src="/assets/images/shop/avatar-1.jpg" alt="avatar" width={100} height={100} /></div>
                         <div className="comment-content clearfix">
                           <div className="comment-author font-alt"><a href="#">John Doe</a></div>
                           <div className="comment-body">
@@ -100,9 +119,9 @@ export default function AllProductsPage() {
                         </div>
                       </div>
                       <div className="comment clearfix">
-                        <div className="comment-avatar"><img src="" alt="avatar"></div>
+                        <div className="comment-avatar"><Image src="/assets/images/shop/avatar-2.jpg" alt="avatar" width={100} height={100} /></div>
                         <div className="comment-content clearfix">
-                          <div className="comment-author font-alt"><a href="#">Mark Stone</a></div>
+                          <div className="comment-author font-alt"><a href="#">MarkStone</a></div>
                           <div className="comment-body">
                             <p>Europe uses the same vocabulary. The European languages are members of the same family. Their separate existence is a myth.</p>
                           </div>
@@ -117,20 +136,20 @@ export default function AllProductsPage() {
                         <div className="row">
                           <div className="col-sm-4">
                             <div className="form-group">
-                              <label className="sr-only" for="name">Name</label>
-                              <input className="form-control" id="name" type="text" name="name" placeholder="Name">
+                              <label className="sr-only" htmlFor="name">Name</label>
+                              <input className="form-control" id="name" type="text" name="name" placeholder="Name" />
                             </div>
                           </div>
                           <div className="col-sm-4">
                             <div className="form-group">
-                              <label className="sr-only" for="email">Name</label>
-                              <input className="form-control" id="email" type="text" name="email" placeholder="E-mail">
+                              <label className="sr-only" htmlFor="email">Email</label>
+                              <input className="form-control" id="email" type="text" name="email" placeholder="E-mail" />
                             </div>
                           </div>
                           <div className="col-sm-4">
-                            <div className="form-group">
-                              <select className="form-control">
-                                <option selected="true" disabled="">Rating</option>
+                            <div className="form-group">  
+                              <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)} className="form-control">
+                                <option value="0" disabled>Rating</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
@@ -141,7 +160,7 @@ export default function AllProductsPage() {
                           </div>
                           <div className="col-sm-12">
                             <div className="form-group">
-                              <textarea className="form-control" id="" name="" rows="4" placeholder="Review"></textarea>
+                              <textarea className="form-control" id="" name="" rows={4} placeholder="Review"></textarea>
                             </div>
                           </div>
                           <div className="col-sm-12">
@@ -153,6 +172,53 @@ export default function AllProductsPage() {
                   </div>
                 </div>
               </div>
+
+              <div className="w-full">
+  <div className="relative right-0">
+    <ul className="relative flex flex-wrap px-1.5 py-1.5 list-none rounded-md bg-slate-100" data-tabs="tabs" role="list">
+      <li className="z-30 flex-auto text-center">
+        <a className="z-30 flex items-center justify-center w-full px-0 py-2 text-sm mb-0 transition-all ease-in-out border-0 rounded-md cursor-pointer text-slate-600 bg-inherit"
+        data-tab-target="" role="tab" aria-selected="true" aria-controls="dashboard">
+          Dashboard
+        </a>
+      </li>
+      <li className="z-30 flex-auto text-center">
+        <a className="z-30 flex items-center justify-center w-full px-0 py-2 mb-0 text-sm transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-600 bg-inherit"
+        data-tab-target="" role="tab" aria-selected="false"  aria-controls="profile">
+          Profile
+        </a>
+      </li>
+      <li className="z-30 flex-auto text-center">
+        <a className="z-30 flex items-center justify-center w-full px-0 py-2 text-sm mb-0 transition-all ease-in-out border-0 rounded-lg cursor-pointer text-slate-700 bg-inherit"
+        data-tab-target="" role="tab" aria-selected="false" aria-controls="settings">
+          Settings
+        </a>
+      </li>
+    </ul>
+ 
+    <div data-tab-content="" className="p-5">
+      <div id="dashboard" role="tabpanel">
+        <p className="text-slate-400 font-light">
+          Because it&apos;s about motivating the doers. Because I&apos;m
+          here to follow my dreams and inspire other people to follow their
+          dreams, too.
+        </p>
+      </div>
+      <div className="hidden opacity-0" id="profile" role="tabpanel">
+        <p className="text-slate-400 font-light">
+          The reading of all good books is like a conversation with the
+          finest minds of past centuries.
+        </p>
+      </div>
+      <div className="hidden opacity-0" id="settings" role="tabpanel">
+        <p className="text-slate-400 font-light">
+          Comparing yourself to others is the thief of joy.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
             </div>
           </div>
         </section>
@@ -160,12 +226,11 @@ export default function AllProductsPage() {
           <div className="max-w-[42rem] m-auto inline">
               <div className="flex m-0 p-0 text-center justify-center items-center">
                 <div className="w-full m-0 p-0 border border-gray-300"></div>
-                  <i className="fa fa-star-o">Button</i>
+                  <i className="fa fa-star-o">*</i>
                 <div className="w-full m-0 p-0 border border-rose-300"></div>
               </div>
           </div>
-          
-
+        
         </div>
     </div>
   )
