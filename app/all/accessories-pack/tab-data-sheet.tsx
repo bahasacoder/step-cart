68553fff070a33ab82c0d10928be12f1e63c1183
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-08-20 22:10:18 +0800

    Add tabDataSheet component for displaying data sheet

---
+++ app/all/accessories-pack/tab-data-sheet.tsx
@@ -0,0 +1,34 @@
+export default function tabDataSheet() {
+  return (
+    <>
+        <div className="tab-pane" id="data-sheet">
+                    <table className="table table-striped ds-table table-responsive">
+                      <tbody>
+                        <tr>
+                          <th>Title</th>
+                          <th>Info</th>
+                        </tr>
+                        <tr>
+                          <td>Compositions</td>
+                          <td>Jeans</td>
+                        </tr>
+                        <tr>
+                          <td>Size</td>
+                          <td>44, 46, 48</td>
+                        </tr>
+                        <tr>
+                          <td>Color</td>
+                          <td>Black</td>
+                        </tr>
+                        <tr>
+                          <td>Brand</td>
+                          <td>Somebrand</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+    </>
+    
+  )
+  
+}
