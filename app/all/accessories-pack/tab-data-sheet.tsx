168d39a361074c4d00e22f434df611f33a2b814b
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-08-22 22:27:33 +0800

    Add product details table to data sheet
    Added a data table with product details including compositions, size, color, and brand.

--- app/all/accessories-pack/tab-data-sheet.tsx
+++ app/all/accessories-pack/tab-data-sheet.tsx
@@ -3,8 +3,32 @@ export default function tabDataSheet() {
     <>
         <div className="tab-pane w-full bg-green-100" id="data-sheet">
              <p>Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.</p>
-                    <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.</p>
-           </div>
+
+                    <table className="table table-striped ds-table table-responsive w-full">
+                      <tbody>
+                        <tr>
+                          <th>Title</th>
+                          <th>Info</th>
+                        </tr>
+                        <tr>
+                          <td>Compositions</td>
+                          <td>Jeans</td>
+                        </tr>
+                        <tr>
+                          <td>Size</td>
+                          <td>44, 46, 48</td>
+                        </tr>
+                        <tr>
+                          <td>Color</td>
+                          <td>Black</td>
+                        </tr>
+                        <tr>
+                          <td>Brand</td>
+                          <td>Somebrand</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                            </div>
     </>
     
   )
