14d27d25c5fea22ee39cc6b9e58dc30df2ae1828
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-08-23 22:28:52 +0800

    Change table background color to rose-200

--- app/all/accessories-pack/tab-data-sheet.tsx
+++ app/all/accessories-pack/tab-data-sheet.tsx
@@ -4,7 +4,7 @@ export default function tabDataSheet() {
         <div className="tab-pane w-full bg-green-100" id="data-sheet">
              <p>Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.</p>
 
-                    <table className="table table-striped ds-table table-responsive w-full">
+                    <table className="table table-striped ds-table table-responsive w-full bg-rose-200">
                       <tbody>
                         <tr>
                           <th>Title</th>
