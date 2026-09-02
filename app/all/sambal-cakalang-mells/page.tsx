75a43d080478c5a11715cb132ad4254496730b4f
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-02 22:06:32 +0800

    Create Sambal Cakalang Mells page component

---
+++ app/all/sambal-cakalang-mells/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Ling from "next/link"
+
+export default function SambalCakalangMellsPage() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to My Sambal Cakalang Mells Site</h1>
+      <p>This is a basic home page.</p>
+    </main>
+  );
+}
