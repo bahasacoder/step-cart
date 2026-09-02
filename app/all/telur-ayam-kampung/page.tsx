b44ec86b030662960f93b8a8157e81534260ca11
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-02 21:59:57 +0800

    Create TelurAyamKampungPage component

---
+++ app/all/telur-ayam-kampung/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Ling from "next/link"
+
+export default function TelurAyamKampungPage() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to My Telur Ayam Kampung Site</h1>
+      <p>This is a basic home page.</p>
+    </main>
+  );
+}
