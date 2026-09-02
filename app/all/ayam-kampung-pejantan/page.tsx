9a5c5a371419ce9e2132e04b4c9e50bc92ea3a18
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-02 22:00:50 +0800

    Create AyamKampungPejantanPage component

---
+++ app/all/ayam-kampung-pejantan/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Ling from "next/link"
+
+export default function AyamKampungPejantamnPage() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to My Ayam Kampung Pejantan Site</h1>
+      <p>This is a basic home page.</p>
+    </main>
+  );
+}
