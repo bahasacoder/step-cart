cb23b790df8764d96851ca73e524539bb8d73fcb
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 09:14:01 +0800

    Update dialog box text for website service promotion

--- components/blocks/dialog-box-home.tsx
+++ components/blocks/dialog-box-home.tsx
@@ -19,8 +19,8 @@ export default function DialogBoxHome() {
           <DialogContent className="sm:max-w-md">            
              <DialogTitle></DialogTitle>
               <div className="relative w-full h-auto mx-auto my-auto p-4">
-                 <h1 className=" w-full absolute top-0 left-0 text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Kami Juga Melayani Pembuatan Website UMKM</h1>
-                 
+                 <h1 className=" w-full absolute top-0 left-0 text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Bangun Website Anda, Bersama Kami</h1>
+                 <h2 className=" w-full absolute top-0 left-0 text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Disini Melayani Pembuatan Website UMKM</h2>
                 </div>
           </DialogContent>
         </Dialog>
