d3aa68ca0eb9e79bd62e87b7dc647ab42b7b25ea
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 11:41:48 +0800

    Wrap Image component in a div for better layout

--- components/blocks/dialog-box-home.tsx
+++ components/blocks/dialog-box-home.tsx
@@ -20,14 +20,16 @@ export default function DialogBoxHome() {
              <DialogTitle></DialogTitle>
               <div className="relative flex flex-col gap-4 w-full max-h-[500px] mx-auto my-auto p-4">
                  <h1 className=" w-full text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Bangun Website Anda, Bersama Kami</h1>
-                 <Image
-                  src="/images/boxdialog906.jpg"
-                  width={500}
-                  height={500}
-                  alt="Picture of the author"
-                  fill
-                   style={{ objectFit: 'contain' }}
-                />
+                  <div className="w-full flex justify-center items-center m-4 p-4">
+                    <Image
+                        src="/images/boxdialog906.jpg"
+                        width={500}
+                        height={500}
+                        alt="Picture of the author"
+                        fill
+                         style={{ objectFit: 'contain' }}
+                      />
+                  </div>   
                 <h2 className=" w-full text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Disini Melayani Pembuatan Website UMKM</h2>
                 </div>
           </DialogContent>
