5391fad5cb3de89517809875bef39dee3a32a2c4
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 22:27:52 +0800

    Implement Frozen Ayam Kampung page with tabs

--- app/all/frozen-ayam-kampung/page.tsx
+++ app/all/frozen-ayam-kampung/page.tsx
@@ -1,13 +1,68 @@
+"use client"
+import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button"
+
+// import styles from './index.module.css';
+import TabDescription from "./tab-description";
+import TabDataSheet from "./tab-data-sheet";
+import TabReviews from "./tab-reviews";
 import OverviewBlock from "./overview-block"
 
 export default function FrozenAyamKampungPage() {
+  const [quantity, setQuantity] = useState(5);
+  const [selectedOption, setSelectedOption] = useState("0");
+  
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Welcome to My Frozen Ayam Kampung Site</h1>
       <p>This is a basic home page.</p>
       <OverviewBlock />
+
+       {/* Product Tabs */}
+            <div className="row">
+              <div className="col-sm-12">
+                 <Tabs className="w-full flex flex-col gap-3" defaultValue="description">
+                    <TabsList className="w-full flex p-4" variant="line">
+                      <TabsTrigger className="relative flex py-4 transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-900" value="description">
+                        Description
+                      </TabsTrigger>
+                      <TabsTrigger className="relative flex py-4 transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-900" value="datasheet">
+                        Data Sheet
+                      </TabsTrigger>
+                      <TabsTrigger className="relative flex py-4 transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-900" value="reviews">
+                        Reviews (2)
+                      </TabsTrigger>
+                    </TabsList>
+                      <TabsContent className="" value="description">
+                        <TabDescription />
+                      </TabsContent>
+                      <TabsContent className="" value="datasheet">
+                        <TabDataSheet />
+                      </TabsContent>
+                      <TabsContent className="" value="reviews">
+                        <TabReviews />
+                      </TabsContent>
+                  </Tabs>
+              </div>         
+            </div>
+            
+            
     </main>
   );
 }
