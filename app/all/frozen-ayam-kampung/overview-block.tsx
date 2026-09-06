2885218af89bab81615f215b245c67eb83904ae7
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 21:41:56 +0800

    Refactor Image component in overview-block.tsx
    Updated Image component to use width and height props for better performance.

--- app/all/frozen-ayam-kampung/overview-block.tsx
+++ app/all/frozen-ayam-kampung/overview-block.tsx
@@ -44,8 +44,8 @@ export default function OverviewBlock() {
                                                   
                                                   src="https://cdn.shadcnstudio.com/ss-assets/blocks/ecommerce/product-overview/image-1.png" 
                                                   alt="Descriptive text"
-                                                  fill // 2. Tells the image to take up 100% of the parent
-                                                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // 3. Optimizes performance
+                                                  width={100}
+                                                  height={100}
                                                   className="object-contain" // 4. Applies object-fit: contain via Tailwind
                                                   />
                                           </SwiperSlide>
