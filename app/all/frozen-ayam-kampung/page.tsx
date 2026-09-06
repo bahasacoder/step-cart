2040d04702dc194fee86f6f8d8a0c868a0c6d770
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 16:31:22 +0800

    Import OverviewBlock and add to FrozenAyamKampungPage

--- app/all/frozen-ayam-kampung/page.tsx
+++ app/all/frozen-ayam-kampung/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import Ling from "next/link"
+import Link from "next/link"
+import OverviewBlock from "./overview-block"
 
 export default function FrozenAyamKampungPage() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Welcome to My Frozen Ayam Kampung Site</h1>
       <p>This is a basic home page.</p>
+      <OverviewBlock />
     </main>
   );
 }
