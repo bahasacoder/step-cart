b7758f3dd4db32adb066720ce39fe2acb49cd24f
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 11:43:47 +0800

    Remove unnecessary z-index from headings

--- components/blocks/dialog-box-home.tsx
+++ components/blocks/dialog-box-home.tsx
@@ -19,7 +19,7 @@ export default function DialogBoxHome() {
           <DialogContent className="sm:max-w-md">            
              <DialogTitle></DialogTitle>
               <div className="relative flex flex-col gap-4 w-full max-h-[500px] mx-auto my-auto p-4">
-                 <h1 className=" w-full text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Bangun Website Anda, Bersama Kami</h1>
+                 <h1 className=" w-full text-8x font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Bangun Website Anda, Bersama Kami</h1>
                   <div className="w-full flex justify-center items-center m-4 p-4">
                     <Image
                         src="/images/boxdialog906.jpg"
@@ -30,7 +30,7 @@ export default function DialogBoxHome() {
                          style={{ objectFit: 'contain' }}
                       />
                   </div>   
-                <h2 className=" w-full text-8x z-10 font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Disini Melayani Pembuatan Website UMKM</h2>
+                <h2 className=" w-full text-8x font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Disini Melayani Pembuatan Website UMKM</h2>
                 </div>
           </DialogContent>
         </Dialog>
