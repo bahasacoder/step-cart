525d95c0909a26c2ce392d07c2bd29feb293b17c
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 22:28:47 +0800

    Add files via upload

---
+++ app/all/frozen-ayam-kampung/tab-data-sheet.tsx
@@ -0,0 +1,36 @@
+export default function tabDataSheet() {
+  return (
+    <>
+        <div className="tab-pane w-full bg-green-100" id="data-sheet">
+             <p>Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words. If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.</p>
+
+                    <table className="table table-striped ds-table table-responsive w-full bg-rose-200">
+                      <tbody>
+                        <tr>
+                          <th>Title</th>
+                          <th>Info</th>
+                        </tr>
+                        <tr>
+                          <td>Compositions</td>
+                          <td>Jeans</td>
+                        </tr>
+                        <tr>
+                          <td>Size</td>
+                          <td>44, 46, 48</td>
+                        </tr>
+                        <tr>
+                          <td>Color</td>
+                          <td>Black</td>
+                        </tr>
+                        <tr>
+                          <td>Brand</td>
+                          <td>Somebrand</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                            </div>
+    </>
+    
+  )
+  
+}
