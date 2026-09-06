68b10594a0af927b3a620a2bd75846249e4e7858
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-06 21:51:40 +0800

    Change image border radius to rounded-lg
    Updated SwiperSlide className to use 'rounded-lg' instead of 'rounded-md'.

--- app/all/frozen-ayam-kampung/overview-block.tsx
+++ app/all/frozen-ayam-kampung/overview-block.tsx
@@ -37,7 +37,7 @@ export default function OverviewBlock() {
                                           modules={[FreeMode, Navigation, Thumbs]}
                                           className="mySwiper2"
                                         >
-                                          <SwiperSlide>
+                                          <SwiperSlide className="bg-none" >
                                            
 
                                                 <Image 
@@ -46,7 +46,7 @@ export default function OverviewBlock() {
                                                   alt="Descriptive text"
                                                   width={100}
                                                   height={100}
-                                                  className="object-contain, rounded-md" // 4. Applies object-fit: contain via Tailwind
+                                                  className="object-contain, rounded-lg" // 4. Applies object-fit: contain via Tailwind
                                                   />
                                           </SwiperSlide>
                                           <SwiperSlide>
