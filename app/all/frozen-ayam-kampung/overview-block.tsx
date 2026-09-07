144897c35ecf86af82367977618daa0b8c0a13a1
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-07 09:19:09 +0800

    Modify image component in overview-block.tsx
    Updated image component to use width and height attributes.

--- app/all/frozen-ayam-kampung/overview-block.tsx
+++ app/all/frozen-ayam-kampung/overview-block.tsx
@@ -48,10 +48,9 @@ export default function OverviewBlock() {
                                           <SwiperSlide>
                                              <Image                                                   
                                                   src="https://raw.githubusercontent.com/bahasacoder/step-cart/refs/heads/main/public/assets/all/frozen-chicken-1.jpg" 
-                                                  alt="Descriptive text" 
-                                                  fill
-                                                  sizes="(max-width: 768px) 100vw, 50vw"
-                                                  style={{ objectFit: 'cover' }} 
+                                                  alt="Descriptive text"                                                  
+                                                  width={100}
+                                                  height={100}
                                                   />
                                           </SwiperSlide>
                                           <SwiperSlide>
