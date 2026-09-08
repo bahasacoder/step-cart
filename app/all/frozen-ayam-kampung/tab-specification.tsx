44ce9cfed7ebfd4ce4e18e9153dac7afcc4ed92d
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-08 19:08:19 +0800

    Add tabSpecification component for poultry details

---
+++ app/all/frozen-ayam-kampung/tab-specification.tsx
@@ -0,0 +1,69 @@
+export default function tabSpecification () {
+
+  return (
+     <div className="ba b--lighter-gray br3 overflow-hidden">
+        <div className="flex w-100" aria-label="Poultry cut: Drumsticks">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Poultry cut</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Drumsticks</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Weight: 4 lb">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Weight</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">4 lb</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Food condition: Frozen">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Food condition</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Frozen</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Shelf life: 270 days">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Shelf life</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">270 days</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Manufacturer: Tyson Foods">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Manufacturer</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Tyson Foods</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Food form: Whole">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Food form</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Whole</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Brand: Great Value">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Brand</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Great Value</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Fresh &amp; frozen poultry type: Chicken">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Fresh &amp; frozen poultry type</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Chicken</div>
+            </div>
+        </div>
+        <hr aria-hidden="true" className="ld_Au">
+        <div className="flex w-100" aria-label="Flavor: Unflavored">
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Flavor</div>
+            <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
+                <div className="b lh-copy">Unflavored</div>
+            </div>
+        </div>
+    </div>
+  )
+}
