9e403f610d85ff0fc2c1acd4e0789be8dc78dad0
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-08 19:52:59 +0800

    Update inline styles to JSX syntax

--- app/all/frozen-ayam-kampung/tab-specification.tsx
+++ app/all/frozen-ayam-kampung/tab-specification.tsx
@@ -4,7 +4,7 @@ export default function tabSpecification () {
     <>
      <div className="ba b--lighter-gray br3 overflow-hidden">
         <div className="flex w-100" aria-label="Poultry cut: Drumsticks">
-            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style="width: 225px; min-width: 225px; background-color: rgb(248, 248, 248);">Poultry cut</div>
+            <div className="f6 lh-copy self-stretch pa3" aria-hidden="true" style={{width: "225px", min-width: "225px", background-color: "rgb(248, 248, 248)"}}>Poultry cut</div>
             <div className="bg-white flex-auto f6 pa3" aria-hidden="true">
                 <div className="b lh-copy">Drumsticks</div>
             </div>
