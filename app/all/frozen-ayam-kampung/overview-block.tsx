d01d0215bbaa9a4228970dd69423cbe8645e16ff
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-08 19:06:05 +0800

    Fix formatting of Image component in overview-block.tsx

--- app/all/frozen-ayam-kampung/overview-block.tsx
+++ app/all/frozen-ayam-kampung/overview-block.tsx
@@ -91,9 +91,9 @@ export default function OverviewBlock() {
                                       <SwiperSlide>
                                         <Image 
                                           src="https://cdn.jsdelivr.net/gh/free-whiteboard-online/Free-Erasorio-Alternative-for-Collaborative-Design@76d2f18cbbb5d6294cad122fbc5b62baf1d4f5ba/uploads/2026-02-12T07-03-40-599Z-su0xe2suz.png" 
-                                           alt="Descriptive thumbnails"
-                                            width={100}
-                                            height={100}
+                                          alt="Descriptive thumbnails"
+                                          width={100}
+                                          height={100}
                                           style={{ width: "7.375rem", height: "4rem"}}
                                           />
                                       </SwiperSlide>
