77587d4dee798c22ac16c941b5f5a479b85fe37b
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-10 10:56:02 +0800

    Remove welcome message from frozen ayam kampung page
    Removed welcome header and introductory paragraph from the page.

--- app/all/frozen-ayam-kampung/page.tsx
+++ app/all/frozen-ayam-kampung/page.tsx
@@ -31,8 +31,7 @@ export default function FrozenAyamKampungPage() {
   
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to My Frozen Ayam Kampung Site</h1>
-      <p>This is a basic home page.</p>
+
       <OverviewBlock />
 
        {/* Product Tabs */}
