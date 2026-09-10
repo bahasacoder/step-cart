bf2e582a219b18644d61f7ed2bb4c16eac94c323
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-10 22:23:55 +0800

    Refactor OverviewBlock component with Swiper integration

--- app/all/frozen/ayam-kampung-250/overview-block.tsx
+++ app/all/frozen/ayam-kampung-250/overview-block.tsx
@@ -133,33 +133,52 @@ export default function OverviewBlock() {
                       <div className="flex items-center gap-6">
                             {/* Variant Label */}
                             <h4 className="text-lg font-semibold whitespace-nowrap">Berat :</h4>                          
-                            {/* Radio Group Wrapper */}
-                            <div role="radiogroup" aria-label="Pilih Berat" className="flex items-center gap-4 w-full">                              
-                              {/* Option 2: Amber / 500 gr */}
-                              <label className="flex items-center gap-2 cursor-pointer group">
-                                <div className="relative flex size-5 items-center justify-center rounded-full bg-amber-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-amber-600/50 peer-checked:ring-2 peer-checked:ring-amber-600">
-                                  {/* Inner dot for active state */}
-                                  <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /> </Link>
+                            {/* Radio Group Wrapper */}                        
+                            <div role="radiogroup" aria-label="Pilih Berat" className="flex items-center gap-4 w-full">
+                              <div role="radiogroup" aria-label="Pilih Berat" className="flex items-center gap-4 w-full">   
+                                  {/* Option 1: Green / 250 gr */}
+                                  <label className="flex items-center gap-2 cursor-pointer group">
+                                    <div className="relative flex size-5 items-center justify-center rounded-full bg-amber-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-amber-600/50 peer-checked:ring-2 peer-checked:ring-amber-600">
+                                      <input 
+                                        type="radio" 
+                                        name="berat" 
+                                        value="250" 
+                                        className="peer sr-only" 
+                                      />
+                                      {/* Inner dot for active state */}
+                                      <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /> </Link>
+                                  
+                                    <span className="text-sm font-medium text-slate-700">250 gr</span>
+                                  </label>
+                               </div>
+                                <div role="radiogroup" aria-label="Pilih Berat" className="flex flex-wrap justify-around items-center gap-4 w-full">   
+                                  {/* Option 2: Amber / 500 gr */}
+                                    <label className="flex items-center gap-2 cursor-pointer group">
+                                      <div className="relative flex size-5 items-center justify-center rounded-full bg-amber-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-amber-600/50 peer-checked:ring-2 peer-checked:ring-amber-600">
+                                        {/* Inner dot for active state */}
+                                        <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /></Link>
+                                      </div>
+                                      <span className="text-sm font-medium text-slate-700">750 gr</span>
+                                    </label>                          
+                                    {/* Option 3: Yellow / 750 gr */}
+                                    <label className="flex items-center gap-2 cursor-pointer group">
+                                      <div className="relative flex size-5 items-center justify-center rounded-full bg-yellow-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-yellow-600/50 peer-checked:ring-2 peer-checked:ring-yellow-600">
+                                        {/* Inner dot for active state */}
+                                        <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /></Link>
+                                      </div>
+                                      <span className="text-sm font-medium text-slate-700">750 gr</span>
+                                    </label>                          
+                                    {/* Option 4: lime / 1000 gr */}
+                                    <label className="flex items-center gap-2 cursor-pointer group">
+                                      <div className="relative flex size-5 items-center justify-center rounded-full bg-lime-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-lime-600/50 peer-checked:ring-2 peer-checked:ring-lime-600">
+                                        {/* Inner dot for active state */}
+                                        <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /></Link>
+                                      </div>
+                                      <span className="text-sm font-medium text-slate-700">1000 gr</span>
+                                    </label>                          
+
                                 </div>
-                                <span className="text-sm font-medium text-slate-700">500 gr</span>
-                              </label>
-                              {/* Option 3: Yellow / 750 gr */}
-                              <label className="flex items-center gap-2 cursor-pointer group">
-                                <div className="relative flex size-5 items-center justify-center rounded-full bg-yellow-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-yellow-600/50 peer-checked:ring-2 peer-checked:ring-yellow-600">
-                                  {/* Inner dot for active state */}
-                                  <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /></Link>
-                                </div>
-                                <span className="text-sm font-medium text-slate-700">750 gr</span>
-                              </label>                          
-                              {/* Option 4: lime / 1000 gr */}
-                              <label className="flex items-center gap-2 cursor-pointer group">
-                                <div className="relative flex size-5 items-center justify-center rounded-full bg-lime-600 shadow-xs transition-all ring-offset-2 group-hover:ring-2 group-hover:ring-lime-600/50 peer-checked:ring-2 peer-checked:ring-lime-600">
-                                  {/* Inner dot for active state */}
-                                  <Link href="/"><div className="hidden size-2 rounded-full bg-white peer-checked:block" /></Link>
-                                </div>
-                                <span className="text-sm font-medium text-slate-700">1000 gr</span>
-                              </label>                          
-                            </div>
+                               </div>
                         </div>  
                       
                         <div className="flex items-center gap-6">
