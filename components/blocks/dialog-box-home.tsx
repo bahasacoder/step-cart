787858f41c75d61f1bb9a510e841aef43fb2af80
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-10 11:25:01 +0800

    Adjust dialog box layout and dimensions

--- components/blocks/dialog-box-home.tsx
+++ components/blocks/dialog-box-home.tsx
@@ -20,7 +20,7 @@ export default function DialogBoxHome() {
              <DialogTitle></DialogTitle>
               <div className="relative flex flex-col gap-4 w-full max-h-[500px] mx-auto my-auto p-4">
                  <h1 className="w-full mx-0 text-center text-lg md:text-4xl font-bold tracking-normal transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue-700 hover:font-bold hover:tracking-wider">Bangun Website Anda, Bersama Kami!</h1>
-                  <div className="relative w-[200px] sm:w-full flex justify-center items-center m-2 p-2">
+                  <div className="relative w-[200px] h-[200px] sm:w-full flex justify-center items-center m-2 p-2">
                     <Image
                         src="/images/boxdialog906.jpg"
                         width={500}
