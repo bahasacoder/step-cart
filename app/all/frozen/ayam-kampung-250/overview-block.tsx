6d3c74df640796dadee7642b037342c1b3ec7fe2
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-12 21:28:08 +0800

    Refactor OverviewBlock component structure and logic

--- app/all/frozen/ayam-kampung-250/overview-block.tsx
+++ app/all/frozen/ayam-kampung-250/overview-block.tsx
@@ -107,7 +107,7 @@ const totalHargaJadi = inputHarga - diskonHarga;
                         </div>
                         <div className="flex items-center gap-3">
                             <p className="text-3xl font-bold">Rp {/*<!-- -->*/}{totalHargaJadi}</p>
-                            <span className="text-muted-foreground font-medium line-through">Rp ${/*<!-- -->*/}{inputHarga}</span>
+                            <span className="text-muted-foreground font-medium line-through">Rp {/*<!-- -->*/}{inputHarga}</span>
                             <span data-slot="badge" data-variant="default" className="cn-badge group/badge focus-visible:border-ring aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 inline-flex w-fit shrink-0 items-center justify-center overflow-hidden whitespace-nowrap focus-visible:ring-[3px] [&amp;&gt;svg]:pointer-events-none cn-badge-variant-default border-none bg-green-600/10 text-green-600 focus-visible:ring-green-600/20 focus-visible:outline-none dark:bg-green-400/10 dark:text-green-400 dark:focus-visible:ring-green-400/40 [a&amp;]:hover:bg-green-600/5 dark:[a&amp;]:hover:bg-green-400/5">
                                 {diskonValue*100}{/*<!-- -->*/}% Off
                             </span>
