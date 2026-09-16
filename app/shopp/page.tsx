3177d788b156262d34a888f83f588b14c1b7f15b
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-16 13:55:25 +0800

    Refactor imports for Cart and Header components

--- app/shopp/page.tsx
+++ app/shopp/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { ProductList } from '@/components/ProductList';
-import { Cart } from '@/components/Cart';
-import { Header } from '@/components/Header';
+import Cart from '@/components/Cart';
+import Header from '@/components/Header';
 import styles from './page.module.css';
 
 export default function ShoppPage() {
