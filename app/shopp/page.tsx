f6cc31d617d6fc6bd19b89abfc8539b52a7902c1
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-16 13:40:19 +0800

    Remove components and add 'Halaman Shopp' text

--- app/shopp/page.tsx
+++ app/shopp/page.tsx
@@ -8,15 +8,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div>
-      <Header />
-      <div className={styles.container}>
-        <div className={styles.mainContent}>
-          <ProductList />
-        </div>
-        <aside className={styles.sidebar}>
-          <Cart />
-        </aside>
-      </div>
+        Halaman Shopp
     </div>
   );
 }
