319a81691d08462d64c80a611b4ec2b56466f993
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-09-16 20:04:58 +0800

    Integrate redux-persist for state persistence

--- lib/store.ts
+++ lib/store.ts
@@ -1,13 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import cartReducer from './features/cart/cartSlice';
 import productReducer from './features/products/productSlice';
 
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  // keranjang: keranjangReducer
+  products: productReducer,
+})
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    products: productReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store)
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export type AppStore = typeof store;
